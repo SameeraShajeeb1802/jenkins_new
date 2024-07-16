265774d0c02e09f8d42c8369aa7d83b26a4d0bc4
Lenovo <Lenovo@DESKTOP-LCIM85U>
2024-07-16 10:06:24 +0530

    pollscm push

---
+++ qspider/src/test/java/jenkins/AddNewClass.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddNewClass {
+	@Test
+	public void add() {
+		Reporter.log("executete successfull",true);
+	}
+
+}
---
+++ qspider/src/test/java/jenkins/ToLauchPollscm.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLauchPollscm {
+	@Test
+	public void launch() {
+		Reporter.log("launch successfull",true);
+	}
+
+}
